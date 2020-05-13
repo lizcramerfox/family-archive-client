418588c23ee2a6f68605c47b35d1777db79e2f86
lizcramerfox <ecfox2008@gmail.com>
2020-05-13 11:28:03 -0400

    All Auth paths tested & working

---
+++ src/components/Memories/MemoryForm.js
@@ -0,0 +1,27 @@
+// import React from 'react'
+// import { Form } from 'react-bootstrap'
+//
+// // Template Form with all fields for Memory
+// const MemoryForm = ({ memory }) => (
+//   <Form>
+//     <Form.Group controlId="memoryTitle">
+//       <Form.Control type="text" placeholder="Enter a title..." />
+//       <Form.Text>Title</Form.Text>
+//     </Form.Group>
+//
+//     <Form.Group controlId="memoryDescription">
+//       <Form.Control type="text" placeholder="Enter a description..." />
+//       <Form.Text>Description</Form.Text>
+//     </Form.Group>
+//
+//     <Form.Group controlId="memoryDescription">
+//       <Form.Control type="text" placeholder="Enter a description..." />
+//       <Form.Text>Description</Form.Text>
+//     </Form.Group>
+//
+//     <Button variant="info">Get Weather</Button>
+//
+//   </Form>
+// )
+//
+// export default MemoryForm
